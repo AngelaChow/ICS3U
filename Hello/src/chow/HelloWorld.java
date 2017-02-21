6dac18864214a8b9c1b9989c50347733bbe1dc37
323990887 <323990887@C-3ZZHYR1.tdsb.on.ca>
2017-02-21 14:46:25 -0500

    Pushing Hello, World!

---
+++ Hello/src/chow/HelloWorld.java
@@ -0,0 +1,16 @@
+package chow;
+/**
+ * HelloWorld.java
+ * Program prints out "Hello, World!"
+ * 2017/02/21
+ * @author Angela Chow
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	 System.out.println("Hello, World!"); 
+
+	}
+
+}
