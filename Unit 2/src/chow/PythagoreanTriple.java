6d8982f85b3c1d3e02801cf39d2792cdefb6fd38
Angela C <Angela C@Angela>
2017-04-20 22:58:55 -0400

    Pushing updated pythagorean triple algorithm

--- Unit 2/src/chow/PythagoreanTriple.java
+++ Unit 2/src/chow/PythagoreanTriple.java
@@ -1,23 +1,36 @@
 package chow;
 
+/**
+ * PythagoreanTriple.java
+ * This program prints out pythagorean triples
+ * 2017/04/20
+ * @author Angela Chow
+ */
+
 public class PythagoreanTriple {
 
 	public static void main(String[] args) {
-		System.out.println("Hello this program will output all the pythagorean triples between 1 and 100.");
-		for(int a = 1; a<100; a++){
-			for (int b = 1; b<100; b++){
-				int c = (int)Math.pow(a,2) + (int)Math.pow(b, 2);
-				if (perfectSquare(c)==true){
-					int x=(int)Math.sqrt(c);
-					System.out.println(a + b + c);	
+		System.out.println("Hello, this program will output pythagorean triples.");
+		for(int a = 1; a< 100; a++){
+			for (int b = 1; b< 100; b++){
+				int c= (int)Math.pow(a,2) + (int)Math.pow(b, 2);
+				if (perfectSquare(c) == true){
+					c = (int)Math.sqrt(c);
+					System.out.println(a + " " + b + " " +c);	
 				}
 			}
 		}
-	}                                                                                                                                             
-	//This method determines if c is a perfect square
-	public static boolean perfectSquare(int x){
-		int check = (int) Math.pow(x,2);
-		if(check==x){
+	}     
+	
+	/**
+	 * This method does a check to see if c is a perfect square
+	 * @param d is the number that is tested for the perfect square from the main method
+	 * @return true if value is a perfect square and false if it isn't
+	 */
+	public static boolean perfectSquare(int d){
+		int x = (int)Math.sqrt(d);
+		double check = (int) Math.pow(x,2);
+		if(check==d){
 			return true;
 		}
 		else{
